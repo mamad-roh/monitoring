0ad4db51c742571563933a0a9670ec9636f133d0
amirHossein <amirzabihpour@gmail.com>
2021-07-10 17:03:32 +0430

    update V2.3

--- media/repository.py
+++ media/repository.py
@@ -23,7 +23,11 @@ def create_media(
         db.commit()
         return {"detail": "Media is added."}
     else:
-        return {'detail': f'Media name must be: {flag}'}
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Media name must be: {flag}'
+        )
+        # return {'detail': f'Media name must be: {flag}'}
 
 
 def get_all_media(db: Session):
